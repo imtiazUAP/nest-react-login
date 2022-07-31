4ee6ea5d320d07987a4981c9229d130932a460e6
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2022-08-01 03:08:13 +0600

    Added & synched db schema

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,20 @@
 import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config: MysqlConnectionOptions = {
     type: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    database: 'typeorm-on-nestjs-mysql',
+    host: process.env.MYSQL_DB_HOST,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
     entities: ['dist/src/**/*.entity.js'],
     migrations: ['dist/migrations/*.js'],
     cli: {
         migrationsDir: 'migrations',
     },
     synchronize: false,
-
+    logging: true,
 }
 
 export default config;
